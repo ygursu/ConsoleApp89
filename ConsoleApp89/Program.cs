69982cae78f22ebe2924c3addcc213ee15f71cb2
ygursu <yasemingrs@gmail.com>
2024-02-24 23:27:22 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp89/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace klavyedenkenarigirileneskenarucgenincevresi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Klavyeden kenarı girilen eşkenar üçgenin çevresini bulan C# programını yazınız.
+
+            Console.WriteLine("Lütfen üçgenin bir kenarını giriniz");
+            int kenar = Int32.Parse(Console.ReadLine());
+
+            int cevre = kenar * 3;
+
+            Console.WriteLine("Üçgenin çevresi=" + cevre);
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+
